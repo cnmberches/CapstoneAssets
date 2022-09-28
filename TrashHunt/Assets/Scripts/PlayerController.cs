c21fbf15bd4eb2f0043fe8387deddb042bb5d252
cnmberches <cnmberches@gmail.com>
2022-09-28 19:43:55 +0800

    Sept 28, 2022

---
+++ TrashHunt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            transform.position = touchPosition;
+            touchPosition.z = 0;
+        }
+    }
+}
